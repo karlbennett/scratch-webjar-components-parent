629f6f47391316ee6022796f2efbce71da47253b
Karl Bennett <karl.bennett.smt@gmail.com>
2017-07-30 18:29:04 +1200

    Simplified the HTML5 login page routing.

--- scratch-webjar-html5/src/main/java/scratch/webjar/html5/RouterConfiguration.java
+++ scratch-webjar-html5/src/main/java/scratch/webjar/html5/RouterConfiguration.java
@@ -1,7 +1,6 @@
 package scratch.webjar.html5;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -15,12 +14,6 @@ public class RouterConfiguration extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/components/page-header").setViewName("/components/page-header");
         registry.addViewController("/components/form-login").setViewName("/components/form-login");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/login")
-            .resourceChain(true)
-            .addResolver(new ClassPathResourceResolver("public/login.html"));
+        registry.addViewController("/login").setViewName("/login.html");
     }
 }
