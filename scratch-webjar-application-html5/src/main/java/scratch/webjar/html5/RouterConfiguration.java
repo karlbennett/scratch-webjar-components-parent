1e36740c06943a009541eedd186f538eb8fc91ea
Karl Bennett <karl.bennett.smt@gmail.com>
2017-08-13 20:47:11 +1200

    The web components are now being utilised completely.

--- scratch-webjar-application-html5/src/main/java/scratch/webjar/html5/RouterConfiguration.java
+++ scratch-webjar-application-html5/src/main/java/scratch/webjar/html5/RouterConfiguration.java
@@ -1,8 +1,12 @@
 package scratch.webjar.html5;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.springframework.http.CacheControl.maxAge;
 
 /**
  * @author Karl Bennett
@@ -12,8 +16,17 @@ public class RouterConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/components/page-header").setViewName("/components/page-header");
-        registry.addViewController("/components/form-login").setViewName("/components/form-login");
+        registry.addViewController("/components/page-login").setViewName("/components/page-login");
         registry.addViewController("/login").setViewName("/login.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").setCacheControl(maxAge(120, SECONDS))
+            .addResourceLocations("classpath:/META-INF/resources/css/");
+        registry.addResourceHandler("/components/**").setCacheControl(maxAge(120, SECONDS))
+            .addResourceLocations("classpath:/public/components/");
+        registry.addResourceHandler("/webjars/**").setCacheControl(maxAge(120, SECONDS))
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
